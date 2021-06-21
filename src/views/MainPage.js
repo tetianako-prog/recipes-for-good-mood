44f576a55ca50790fefa87b1b0ca7942ee11154b
tetianako-prog <t.kobyliakova@gmail.com>
2021-06-21 13:05:29 +0300

    Add margin-top at main page

--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -42,31 +42,3 @@ const MainPage = () => {
 };
 
 export default MainPage;
-
-//  axios.put('https://60ae79925b8c300017dea964.mockapi.io/api/recipes/8', {
-//    name: 'Помідори, фаршировані маслюками і цибулею',
-//    type: 'appetizers',
-//    date: '01 березня 2021',
-//    image:
-//      'https://kuhari.com.ua/wp-content/uploads/2020/04/pomidori_farshirovannie_yaicom_i_lukom-610715.webp',
-//    people: '3 персони',
-//    preparation: '15 хвилин',
-//    time: '20 хвилин',
-//    ingredients: [
-//      'баклажани - 2 шт',
-//      'часник - 2 зубчики',
-//      'часник - 2 зубчики',
-//      'вершкове масло - 30 г',
-//      'базилік',
-//    ],
-//    description:
-//      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde',
-//    steps: {
-//      step1:
-//        'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-//      step2:
-//        'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-//    },
-//    isLiked: false,
-//    id: '8',
-//  });
